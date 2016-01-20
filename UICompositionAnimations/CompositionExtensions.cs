3db3bd748f6ae706ba7865213a5a6d6fc8f6e623
Sergio Pedri <Sergio0694@live.com>
2016-01-20 12:04:07 +0100

    Added scale animations, reverse mode, bug fixes

--- UICompositionAnimations/CompositionExtensions.cs
+++ UICompositionAnimations/CompositionExtensions.cs
@@ -11,6 +11,33 @@ namespace Windows.UI.Composition
     /// </summary>
     public static class CompositionExtensions
     {
+        #region Internal tools
+
+        /// <summary>
+        /// Sets the CenterPoint of a visual to the center of a given FrameworkElement
+        /// </summary>
+        /// <param name="element">The source element</param>
+        /// <param name="visual">The Visual object for the source FrameworkElement</param>
+        private static async Task SetCenterPoint(this FrameworkElement element, Visual visual)
+        {
+            if (element.ActualWidth + element.ActualHeight < 0.1)
+            {
+                // Wait for the loaded event and set the CenterPoint
+                TaskCompletionSource<object> loadedTcs = new TaskCompletionSource<object>();
+                RoutedEventHandler handler = null;
+                handler = (s, e) =>
+                {
+                    loadedTcs.SetResult(null);
+                    element.Loaded -= handler;
+                };
+                element.Loaded += handler;
+                await loadedTcs.Task;
+            }
+            visual.CenterPoint = new Vector3((float)element.ActualWidth / 2, (float)element.ActualHeight / 2, 0);
+        }
+
+        #endregion
+
         #region Fade
 
         // Manages the fade animation
@@ -20,31 +47,26 @@ namespace Windows.UI.Composition
         {
             // Get the default values
             Visual visual = element.GetVisual();
+            visual.StopAnimation("Opacity");
             if (!startOp.HasValue) startOp = visual.Opacity;
 
-            // Move to a background thread
+            // Get the easing function, the duration and delay
+            CompositionEasingFunction ease = visual.GetEasingFunction(easingFunction);
+            TimeSpan duration = TimeSpan.FromMilliseconds(ms);
+            TimeSpan? delay;
+            if (msDelay.HasValue) delay = TimeSpan.FromMilliseconds(msDelay.Value);
+            else delay = null;
+
+            // Get the opacity the animation
+            ScalarKeyFrameAnimation opacityAnimation = visual.Compositor.CreateScalarKeyFrameAnimation(endOp, startOp, duration, delay, ease);
+
+            // Close the batch and manage its event
+            CompositionScopedBatch batch = visual.Compositor.CreateScopedBatch(CompositionBatchTypes.Animation);
             TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
-            return Task.Run(() =>
-            {
-                // Get the easing function, the duration and delay
-                CompositionEasingFunction ease = visual.GetEasingFunction(easingFunction);
-                TimeSpan duration = TimeSpan.FromMilliseconds(ms);
-                TimeSpan? delay;
-                if (msDelay.HasValue) delay = TimeSpan.FromMilliseconds(msDelay.Value);
-                else delay = null;
-
-                // Get the batch
-                CompositionScopedBatch batch = visual.Compositor.CreateScopedBatch(CompositionBatchTypes.Animation);
-
-                // Get the opacity the animation
-                ScalarKeyFrameAnimation opacityAnimation = visual.Compositor.CreateScalarKeyFrameAnimation(endOp, startOp, duration, delay, ease);
-
-                // Close the batch and manage its event
-                batch.End();
-                batch.Completed += (s, e) => tcs.SetResult(null);
-                visual.StartAnimation("Opacity", opacityAnimation);
-                return tcs.Task;
-            });
+            batch.Completed += (s, e) => tcs.SetResult(null);
+            visual.StartAnimation("Opacity", opacityAnimation);
+            batch.End();
+            return tcs.Task;
         }
 
         /// <summary>
@@ -93,49 +115,47 @@ namespace Windows.UI.Composition
         {
             // Get the default values
             Visual visual = element.GetVisual();
+            visual.StopAnimation("Opacity");
+            visual.StopAnimation("Offset");
             if (!startOp.HasValue) startOp = visual.Opacity;
 
-            // Move to a background thread
-            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
-            return Task.Run(() =>
+            // Get the easing function, the duration and delay
+            CompositionEasingFunction ease = visual.GetEasingFunction(easingFunction);
+            TimeSpan duration = TimeSpan.FromMilliseconds(ms);
+            TimeSpan? delay;
+            if (msDelay.HasValue) delay = TimeSpan.FromMilliseconds(msDelay.Value);
+            else delay = null;
+
+            // Calculate the initial and final offset values
+            Vector3 initialOffset = visual.Offset;
+            Vector3 endOffset = visual.Offset;
+            if (axis == TranslationAxis.X)
             {
-                // Get the easing function, the duration and delay
-                CompositionEasingFunction ease = visual.GetEasingFunction(easingFunction);
-                TimeSpan duration = TimeSpan.FromMilliseconds(ms);
-                TimeSpan? delay;
-                if (msDelay.HasValue) delay = TimeSpan.FromMilliseconds(msDelay.Value);
-                else delay = null;
+                if (startXY.HasValue) initialOffset.X = startXY.Value;
+                endOffset.X = endXY;
+            }
+            else
+            {
+                if (startXY.HasValue) initialOffset.Y = startXY.Value;
+                endOffset.Y = endXY;
+            }
 
-                // Calculate the initial and final offset values
-                Vector3 initialOffset = visual.Offset;
-                Vector3 endOffset = visual.Offset;
-                if (axis == TranslationAxis.X)
-                {
-                    if (startXY.HasValue) initialOffset.X = startXY.Value;
-                    endOffset.X = endXY;
-                }
-                else
-                {
-                    if (startXY.HasValue) initialOffset.Y = startXY.Value;
-                    endOffset.Y = endXY;
-                }
+            // Get the batch
+            CompositionScopedBatch batch = visual.Compositor.CreateScopedBatch(CompositionBatchTypes.Animation);
 
-                // Get the batch
-                CompositionScopedBatch batch = visual.Compositor.CreateScopedBatch(CompositionBatchTypes.Animation);
+            // Get the opacity the animation
+            ScalarKeyFrameAnimation opacityAnimation = visual.Compositor.CreateScalarKeyFrameAnimation(endOp, startOp, duration, delay, ease);
 
-                // Get the opacity the animation
-                ScalarKeyFrameAnimation opacityAnimation = visual.Compositor.CreateScalarKeyFrameAnimation(endOp, startOp, duration, delay, ease);
+            // Offset animation
+            Vector3KeyFrameAnimation offsetAnimation = visual.Compositor.CreateVector3KeyFrameAnimation(endOffset, initialOffset, duration, delay, ease);
 
-                // Offset animation
-                Vector3KeyFrameAnimation offsetAnimation = visual.Compositor.CreateVector3KeyFrameAnimation(endOffset, initialOffset, duration, delay, ease);
-
-                // Close the batch and manage its event
-                batch.End();
-                batch.Completed += (s, e) => tcs.SetResult(null);
-                visual.StartAnimation("Opacity", opacityAnimation);
-                visual.StartAnimation("Offset", offsetAnimation);
-                return tcs.Task;
-            });
+            // Close the batch and manage its event
+            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
+            batch.Completed += (s, e) => tcs.SetResult(null);
+            visual.StartAnimation("Opacity", opacityAnimation);
+            visual.StartAnimation("Offset", offsetAnimation);
+            batch.End();
+            return tcs.Task;
         }
 
         /// <summary>
@@ -192,67 +212,50 @@ namespace Windows.UI.Composition
         {
             // Get the default values and set the CenterPoint
             Visual visual = element.GetVisual();
-            if (element.ActualWidth + element.ActualHeight < 0.1)
-            {
-                // Wait for the loaded event and set the CenterPoint
-                TaskCompletionSource<object> loadedTcs = new TaskCompletionSource<object>();
-                RoutedEventHandler handler = null;
-                handler = (s, e) =>
-                {
-                    loadedTcs.SetResult(null);
-                    element.Loaded -= handler;
-                };
-                element.Loaded += handler;
-                await loadedTcs.Task;
-            }
-            visual.CenterPoint = new Vector3((float)element.ActualWidth / 2, (float)element.ActualHeight / 2, 0);
+            visual.StopAnimation("Opacity");
+            visual.StopAnimation("Scale");
+            await element.SetCenterPoint(visual);
             if (!startOp.HasValue) startOp = visual.Opacity;
 
-            // Move to a background thread
-            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
-            await Task.Run(() =>
+            // Get the easing function, the duration and delay
+            CompositionEasingFunction ease = visual.GetEasingFunction(easingFunction);
+            TimeSpan duration = TimeSpan.FromMilliseconds(ms);
+            TimeSpan? delay;
+            if (msDelay.HasValue) delay = TimeSpan.FromMilliseconds(msDelay.Value);
+            else delay = null;
+
+            // Calculate the initial and final scale values
+            Vector3 initialScale = visual.Scale;
+            if (startXY.HasValue)
             {
-                // Get the easing function, the duration and delay
-                CompositionEasingFunction ease = visual.GetEasingFunction(easingFunction);
-                TimeSpan duration = TimeSpan.FromMilliseconds(ms);
-                TimeSpan? delay;
-                if (msDelay.HasValue) delay = TimeSpan.FromMilliseconds(msDelay.Value);
-                else delay = null;
+                initialScale.X = startXY.Value;
+                initialScale.Y = startXY.Value;
+            }
+            Vector3 endScale = new Vector3
+            {
+                X = endXY,
+                Y = endXY,
+                Z = visual.Scale.Z
+            };
 
-                // Calculate the initial and final scale values
-                Vector3 initialScale = visual.Scale;
-                if (startXY.HasValue)
-                {
-                    initialScale.X = startXY.Value;
-                    initialScale.Y = startXY.Value;
-                }
-                Vector3 endScale = new Vector3
-                {
-                    X = endXY,
-                    Y = endXY,
-                    Z = visual.Scale.Z
-                };
+            // Get the opacity the animation
+            ScalarKeyFrameAnimation opacityAnimation = visual.Compositor.CreateScalarKeyFrameAnimation(endOp, startOp, duration, delay, ease);
 
-                // Get the batch
-                CompositionScopedBatch batch = visual.Compositor.CreateScopedBatch(CompositionBatchTypes.Animation);
+            // Scale animation
+            Vector3KeyFrameAnimation offsetAnimation = visual.Compositor.CreateVector3KeyFrameAnimation(endScale, initialScale, duration, delay, ease);
 
-                // Get the opacity the animation
-                ScalarKeyFrameAnimation opacityAnimation = visual.Compositor.CreateScalarKeyFrameAnimation(endOp, startOp, duration, delay, ease);
-
-                // Offset animation
-                Vector3KeyFrameAnimation offsetAnimation = visual.Compositor.CreateVector3KeyFrameAnimation(endScale, initialScale, duration, delay, ease);
-
-                // Close the batch and manage its event
-                batch.End();
-                batch.Completed += (s, e) => tcs.SetResult(null);
-                visual.StartAnimation("Opacity", opacityAnimation);
-                visual.StartAnimation("Scale", offsetAnimation);
-                return tcs.Task;
-            });
+            // Get the batch and start the animations
+            CompositionScopedBatch batch = visual.Compositor.CreateScopedBatch(CompositionBatchTypes.Animation);
+            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
+            batch.Completed += (s, e) => tcs.SetResult(null);
+            visual.StartAnimation("Opacity", opacityAnimation);
+            visual.StartAnimation("Scale", offsetAnimation);
+            batch.End();
+            await tcs.Task;
         }
 
         /// <summary>
-        /// Starts a fade and slide animation on the target FrameworkElement and optionally runs a callback Action when the animations finish
+        /// Starts a fade and scale animation on the target FrameworkElement and optionally runs a callback Action when the animations finish
         /// </summary>
         /// <param name="element">The UIElement to animate</param>
         /// <param name="startOp">The initial opacity value. If null, the current opacity will be used</param>
@@ -273,7 +276,7 @@ namespace Windows.UI.Composition
         }
 
         /// <summary>
-        /// Starts a fade and slide animation on the target FrameworkElement and returns a Task that completes when the animation ends
+        /// Starts a fade and scale animation on the target FrameworkElement and returns a Task that completes when the animation ends
         /// </summary>
         /// <param name="element">The UIElement to animate</param>
         /// <param name="startOp">The initial opacity value. If null, the current opacity will be used</param>
@@ -293,10 +296,101 @@ namespace Windows.UI.Composition
 
         #endregion
 
+        #region Scale only
+
+        // Manages the scale animation
+        private static async Task<float> ManageCompositionFadeScaleAnimationAsync(this FrameworkElement element,
+            float? startXY, float endXY,
+            int ms, int? msDelay, EasingFunctionNames easingFunction)
+        {
+            // Get the default values and set the CenterPoint
+            Visual visual = element.GetVisual();
+            visual.StopAnimation("Scale");
+            await element.SetCenterPoint(visual);
+
+            // Get the easing function, the duration and delay
+            CompositionEasingFunction ease = visual.GetEasingFunction(easingFunction);
+            TimeSpan duration = TimeSpan.FromMilliseconds(ms);
+            TimeSpan? delay;
+            if (msDelay.HasValue) delay = TimeSpan.FromMilliseconds(msDelay.Value);
+            else delay = null;
+
+            // Calculate the initial and final scale values
+            Vector3 initialScale = visual.Scale;
+            if (startXY.HasValue)
+            {
+                initialScale.X = startXY.Value;
+                initialScale.Y = startXY.Value;
+            }
+            Vector3 endScale = new Vector3
+            {
+                X = endXY,
+                Y = endXY,
+                Z = visual.Scale.Z
+            };
+
+            // Scale animation
+            Vector3KeyFrameAnimation offsetAnimation = visual.Compositor.CreateVector3KeyFrameAnimation(endScale, initialScale, duration, delay, ease);
+
+            // Get the batch and start the animations
+            CompositionScopedBatch batch = visual.Compositor.CreateScopedBatch(CompositionBatchTypes.Animation);
+            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
+            batch.Completed += (s, e) => tcs.SetResult(null);
+            visual.StartAnimation("Scale", offsetAnimation);
+            batch.End();
+            await tcs.Task;
+            return initialScale.X;
+        }
+
+        /// <summary>
+        /// Starts a scale animation on the target FrameworkElement and optionally runs a callback Action when the animations finish
+        /// </summary>
+        /// <param name="element">The UIElement to animate</param>
+        /// <param name="startScale">The initial scale X and Y value. If null, the current scale will be used</param>
+        /// <param name="endScale">The final scale X and Y value</param>
+        /// <param name="ms">The duration of the animation, in milliseconds</param>
+        /// <param name="msDelay">The delay before the animation starts, in milliseconds. If null, there will be no delay</param>
+        /// <param name="easingFunction">The easing function to use with the new animations</param>
+        /// <param name="reverse">If true, the animation will be played in reverse mode when it finishes for the first time</param>
+        /// <param name="callback">An Action to execute when the new animations end</param>
+        public static async void StartCompositionScaleAnimation(this FrameworkElement element,
+            float? startScale, float endScale,
+            int ms, int? msDelay, EasingFunctionNames easingFunction, bool reverse = false, Action callback = null)
+        {
+            await element.StartCompositionScaleAnimationAsync(startScale, endScale, ms, msDelay, easingFunction, reverse);
+            callback?.Invoke();
+        }
+
+        /// <summary>
+        /// Starts a scale animation on the target FrameworkElement and returns a Task that completes when the animation ends
+        /// </summary>
+        /// <param name="element">The UIElement to animate</param>
+        /// <param name="startScale">The initial scale X and Y value. If null, the current scale will be used</param>
+        /// <param name="endScale">The final scale X and Y value</param>
+        /// <param name="ms">The duration of the animation, in milliseconds</param>
+        /// <param name="msDelay">The delay before the animation starts, in milliseconds. If null, there will be no delay</param>
+        /// <param name="easingFunction">The easing function to use with the new animations</param>
+        /// <param name="reverse">If true, the animation will be played in reverse mode when it finishes for the first time</param>
+        public static async Task StartCompositionScaleAnimationAsync(this FrameworkElement element,
+            float? startScale, float endScale,
+            int ms, int? msDelay, EasingFunctionNames easingFunction, bool reverse = false)
+        {
+            startScale = await ManageCompositionFadeScaleAnimationAsync(element, startScale, endScale, ms, msDelay, easingFunction);
+            if (reverse) await ManageCompositionFadeScaleAnimationAsync(element, endScale, startScale.Value, ms, msDelay, easingFunction);
+        }
+
+        #endregion
+
         #region Utility extensions
 
         /// <summary>
-        /// Sets the opacity for the Visual object behing a given UIElement
+        /// Gets the opacity for the Visual object behind a given UIElement
+        /// </summary>
+        /// <param name="element">The source UIElement</param>
+        public static float GetVisualOpacity(this UIElement element) => element.GetVisual().Opacity;
+
+        /// <summary>
+        /// Sets the opacity for the Visual object behind a given UIElement
         /// </summary>
         /// <param name="element">The source UIElement</param>
         /// <param name="value">The new opacity value</param>
