073fbce63ae4624761543390fcebd7b6e72fcdc2
Sergio Pedri <Sergio0694@live.com>
2017-07-17 02:16:52 +0200

    Major refactoring, new LightsSource attached property

--- UICompositionAnimations/Helpers/PointerEvents/PointerHelper.cs
+++ UICompositionAnimations/Helpers/PointerEvents/PointerHelper.cs
@@ -2,8 +2,9 @@
 using Windows.Devices.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Input;
+using JetBrains.Annotations;
 
-namespace UICompositionAnimations.Helpers
+namespace UICompositionAnimations.Helpers.PointerEvents
 {
     /// <summary>
     /// A static class with some extension methods to manage different pointer states
@@ -16,51 +17,61 @@ namespace UICompositionAnimations.Helpers
         /// <param name="control">The control to monitor</param>
         /// <param name="action">An action to call every time a pointer event is raised. The bool parameter
         /// indicates whether the pointer is moving to or from the control</param>
-        public static void ManageControlPointerStates(this UIElement control, Action<PointerDeviceType, bool> action)
+        [PublicAPI]
+        [NotNull]
+        public static ControlAttachedHandlersInfo ManageControlPointerStates(this UIElement control, Action<PointerDeviceType, bool> action)
         {
             // Nested functions that adds the actual handlers
-            void AddHandler(RoutedEvent @event, bool state, Func<PointerDeviceType, bool> predicate)
+            PointerHandlerInfo AddHandler(RoutedEvent @event, bool state, Func<PointerDeviceType, bool> predicate)
             {
-                control.AddHandler(@event, new PointerEventHandler((_, e) =>
+                PointerEventHandler handler = (_, e) =>
                 {
                     if (predicate == null || predicate(e.Pointer.PointerDeviceType))
                     {
                         action(e.Pointer.PointerDeviceType, state);
                     }
-                }), true);
+                };
+                control.AddHandler(@event, handler, true);
+                return new PointerHandlerInfo(@event, handler);
             }
 
             // Add handlers
-            AddHandler(UIElement.PointerExitedEvent, false, null);
-            AddHandler(UIElement.PointerCaptureLostEvent, false, null);
-            AddHandler(UIElement.PointerCanceledEvent, false, null);
-            AddHandler(UIElement.PointerEnteredEvent, true, p => p != PointerDeviceType.Touch);
-            AddHandler(UIElement.PointerReleasedEvent, false, p => p == PointerDeviceType.Touch);
+            return new ControlAttachedHandlersInfo(control,
+                AddHandler(UIElement.PointerExitedEvent, false, null),
+                AddHandler(UIElement.PointerCaptureLostEvent, false, null),
+                AddHandler(UIElement.PointerCanceledEvent, false, null),
+                AddHandler(UIElement.PointerEnteredEvent, true, p => p != PointerDeviceType.Touch),
+                AddHandler(UIElement.PointerReleasedEvent, false, p => p == PointerDeviceType.Touch));
         }
 
         /// <summary>
         /// Adds an event handler to all the pointer events of the target element
         /// </summary>
-        /// <param name="element">The element to monitor</param>
+        /// <param name="host">The element to monitor</param>
         /// <param name="action">An action to call every time a pointer event is raised. The bool parameter
         /// indicates whether the pointer is moving to or from the control</param>
-        public static void ManageHostPointerStates(this UIElement element, Action<PointerDeviceType, bool> action)
+        [PublicAPI]
+        [NotNull]
+        public static ControlAttachedHandlersInfo ManageHostPointerStates(this UIElement host, Action<PointerDeviceType, bool> action)
         {
             // Nested functions that adds the actual handlers
-            void AddHandler(RoutedEvent @event, bool state, Func<PointerDeviceType, bool> predicate)
+            PointerHandlerInfo AddHandler(RoutedEvent @event, bool state, Func<PointerDeviceType, bool> predicate)
             {
-                element.AddHandler(@event, new PointerEventHandler((_, e) =>
+                PointerEventHandler handler = (_, e) =>
                 {
                     if (predicate == null || predicate(e.Pointer.PointerDeviceType))
                     {
                         action(e.Pointer.PointerDeviceType, state);
                     }
-                }), true);
+                };
+                host.AddHandler(@event, handler, true);
+                return new PointerHandlerInfo(@event, handler);
             }
 
             // Add handlers
-            AddHandler(UIElement.PointerExitedEvent, false, null);
-            AddHandler(UIElement.PointerMovedEvent, true, p => p != PointerDeviceType.Touch);
+            return new ControlAttachedHandlersInfo(host,
+                AddHandler(UIElement.PointerExitedEvent, false, null),
+                AddHandler(UIElement.PointerMovedEvent, true, p => p != PointerDeviceType.Touch));
         }
 
         /// <summary>
@@ -68,13 +79,15 @@ namespace UICompositionAnimations.Helpers
         /// </summary>
         /// <param name="element">The element to monitor</param>
         /// <param name="action">An action to call every time the light effects state should be changed</param>
-        public static void ManageLightsPointerStates(this UIElement element, Action<bool> action)
+        [PublicAPI]
+        [CanBeNull]
+        public static ControlAttachedHandlersInfo ManageLightsPointerStates(this UIElement element, Action<bool> action)
         {
             // Platform check
-            if (ApiInformationHelper.IsMobileDevice) return;
+            if (ApiInformationHelper.IsMobileDevice) return null;
 
             // Nested functions that adds the actual handlers
-            element.ManageHostPointerStates((pointer, value) =>
+            return element.ManageHostPointerStates((pointer, value) =>
             {
                 if (pointer != PointerDeviceType.Mouse) return;
                 action(value);
