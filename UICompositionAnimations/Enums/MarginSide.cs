0d4f68e4115bb523745c43f10ce637dbea56c7a4
Sergio0694 <Sergio0694@live.com>
2018-02-11 16:23:18 +0100

    NuGet info updated, minor tweaks

--- UICompositionAnimations/Enums/MarginSide.cs
+++ UICompositionAnimations/Enums/MarginSide.cs
@@ -5,9 +5,24 @@
     /// </summary>
     public enum MarginSide
     {
+        /// <summary>
+        /// Maps the top side of the target margin
+        /// </summary>
         Top,
+
+        /// <summary>
+        /// Maps the bottom side of the target margin
+        /// </summary>
         Bottom,
+
+        /// <summary>
+        /// Maps the left side of the target margin
+        /// </summary>
         Left,
+
+        /// <summary>
+        /// Maps the right side of the target margin
+        /// </summary>
         Right
     }
 }
