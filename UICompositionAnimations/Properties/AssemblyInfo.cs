8414ff28cb19dd159d934f8b6f378e95d54a7ddc
Sergio0694 <Sergio0694@live.com>
2018-02-16 11:34:00 +0100

    NuGet info updated

--- UICompositionAnimations/Properties/AssemblyInfo.cs
+++ UICompositionAnimations/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.14.0.0")]
-[assembly: AssemblyInformationalVersion("2.14.0.0-preview3")]
+[assembly: AssemblyInformationalVersion("2.14.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
