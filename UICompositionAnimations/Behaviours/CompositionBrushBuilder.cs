93cc80603c8d5e7e38992261fda63473de23c60f
Sergio Pedri <sergio0694@live.com>
2018-09-04 02:52:14 +0200

    More API overloads added

--- UICompositionAnimations/Behaviours/CompositionBrushBuilder.cs
+++ UICompositionAnimations/Behaviours/CompositionBrushBuilder.cs
@@ -229,6 +229,25 @@ namespace UICompositionAnimations.Behaviours
         }
 
         /// <summary>
+        /// Returns a new <see cref="CompositionBrushBuilder"/> instance that implements the host backdrop acrylic effect
+        /// </summary>
+        /// <param name="tint">The tint color to use</param>
+        /// <param name="tintAnimation">The animation to apply on the tint color of the effect</param>
+        /// <param name="mix">The amount of tint to apply over the current effect</param>
+        /// <param name="noiseUri">The <see cref="Uri"/> for the noise texture to load for the acrylic effect</param>
+        /// <param name="cache">The cache mode to use to load the image</param>
+        [Pure, NotNull]
+        public static CompositionBrushBuilder FromHostBackdropAcrylic(Color tint, float mix, out EffectAnimation tintAnimation, [NotNull] Uri noiseUri, BitmapCacheMode cache = BitmapCacheMode.Default)
+        {
+            return FromHostBackdropBrush()
+                .Effect(source => new LuminanceToAlphaEffect { Source = source })
+                .Opacity(0.4f)
+                .Blend(FromHostBackdropBrush(), BlendEffectMode.Multiply)
+                .Tint(tint, mix, out tintAnimation)
+                .Blend(FromTiles(noiseUri, cache: cache), BlendEffectMode.Overlay, EffectPlacement.Background);
+        }
+
+        /// <summary>
         /// Returns a new <see cref="CompositionBrushBuilder"/> instance that implements the in-app backdrop acrylic effect
         /// </summary>
         /// <param name="tint">The tint color to use</param>
@@ -242,6 +261,48 @@ namespace UICompositionAnimations.Behaviours
             return FromBackdropBrush()
                 .Tint(tint, mix)
                 .Blur(blur)
+                .Blend(FromTiles(noiseUri, cache: cache), BlendEffectMode.Overlay, EffectPlacement.Background);
+        }
+
+        /// <summary>
+        /// Returns a new <see cref="CompositionBrushBuilder"/> instance that implements the in-app backdrop acrylic effect
+        /// </summary>
+        /// <param name="tint">The tint color to use</param>
+        /// <param name="mix">The amount of tint to apply over the current effect</param>
+        /// <param name="tintAnimation">The animation to apply on the tint color of the effect</param>
+        /// <param name="blur">The amount of blur to apply to the acrylic brush</param>
+        /// <param name="noiseUri">The <see cref="Uri"/> for the noise texture to load for the acrylic effect</param>
+        /// <param name="cache">The cache mode to use to load the image</param>
+        [Pure, NotNull]
+        public static CompositionBrushBuilder FromBackdropAcrylic(
+            Color tint, float mix, out EffectAnimation tintAnimation,
+            float blur,
+            [NotNull] Uri noiseUri, BitmapCacheMode cache = BitmapCacheMode.Default)
+        {
+            return FromBackdropBrush()
+                .Tint(tint, mix, out tintAnimation)
+                .Blur(blur)
+                .Blend(FromTiles(noiseUri, cache: cache), BlendEffectMode.Overlay, EffectPlacement.Background);
+        }
+
+        /// <summary>
+        /// Returns a new <see cref="CompositionBrushBuilder"/> instance that implements the in-app backdrop acrylic effect
+        /// </summary>
+        /// <param name="tint">The tint color to use</param>
+        /// <param name="mix">The amount of tint to apply over the current effect</param>
+        /// <param name="blur">The amount of blur to apply to the acrylic brush</param>
+        /// <param name="blurAnimation">The animation to apply on the blur effect in the pipeline</param>
+        /// <param name="noiseUri">The <see cref="Uri"/> for the noise texture to load for the acrylic effect</param>
+        /// <param name="cache">The cache mode to use to load the image</param>
+        [Pure, NotNull]
+        public static CompositionBrushBuilder FromBackdropAcrylic(
+            Color tint, float mix,
+            float blur, out EffectAnimation blurAnimation,
+            [NotNull] Uri noiseUri, BitmapCacheMode cache = BitmapCacheMode.Default)
+        {
+            return FromBackdropBrush()
+                .Tint(tint, mix)
+                .Blur(blur, out blurAnimation)
                 .Blend(FromTiles(noiseUri, cache: cache), BlendEffectMode.Overlay, EffectPlacement.Background);
         }
 
