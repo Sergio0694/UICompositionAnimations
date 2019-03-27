944ddf2d5c1e2256b54cf2c957ca49dc0fab35e2
Sergio Pedri <sergio0694@live.com>
2019-03-27 21:46:21 +0100

    New helper types added

---
+++ UICompositionAnimations/Extensions/System.Threading.Tasks/TaskCompletionSource.cs
@@ -0,0 +1,18 @@
+﻿namespace System.Threading.Tasks
+{
+    /// <summary>
+    /// A <see cref="TaskCompletionSource{TResult}"/> implementation without the generic argument
+    /// </summary>
+    public sealed class TaskCompletionSource : TaskCompletionSource<Unit>
+    {
+        /// <summary>
+        /// Transitions the underlying <see cref="Task{TResult}"/> into the <see cref="TaskStatus.RanToCompletion"/> state
+        /// </summary>
+        public void SetResult() => SetResult(Unit.Value);
+
+        /// <summary>
+        /// Attempts to transition the underlying <see cref="Task{TResult}"/> into the <see cref="TaskStatus.RanToCompletion"/> state
+        /// </summary>
+        public void TrySetResult() => TrySetResult(Unit.Value);
+    }
+}
