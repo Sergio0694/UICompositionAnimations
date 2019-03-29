b0f0116c61f4cd43c0248e8f052fc2286e9aac16
Sergio Pedri <sergio0694@live.com>
2019-03-29 22:24:53 +0100

    Fixed XAML size animation with NaN starting value

--- UICompositionAnimations/Animations/XamlAnimationBuilder.cs
+++ UICompositionAnimations/Animations/XamlAnimationBuilder.cs
@@ -82,7 +82,16 @@ namespace UICompositionAnimations.Animations
         /// <inheritdoc/>
         protected override IAnimationBuilder OnSize(Axis axis, double? from, double to, Easing ease)
         {
-            AnimationFactories.Add(duration => TargetElement.CreateDoubleAnimation(axis == Axis.X ? nameof(FrameworkElement.Width) : nameof(FrameworkElement.Height), from, to, duration, ease, true));
+            if (!(TargetElement is FrameworkElement element)) throw new InvalidOperationException("Can't animate the size of an item that's not a framework element");
+            AnimationFactories.Add(duration =>
+            {
+                switch (axis)
+                {
+                    case Axis.X: return TargetElement.CreateDoubleAnimation(nameof(FrameworkElement.Width), double.IsNaN(element.Width) ? element.ActualWidth : from, to, duration, ease, true);
+                    case Axis.Y: return TargetElement.CreateDoubleAnimation(nameof(FrameworkElement.Height), double.IsNaN(element.Height) ? element.ActualHeight : from, to, duration, ease, true);
+                    default: throw new ArgumentException($"Invalid axis: {axis}", nameof(axis));
+                }
+            });
 
             return this;
         }
@@ -90,8 +99,12 @@ namespace UICompositionAnimations.Animations
         /// <inheritdoc/>
         protected override IAnimationBuilder OnSize(Vector2? from, Vector2 to, Easing ease = Easing.Linear)
         {
-            AnimationFactories.Add(duration => TargetTransform.CreateDoubleAnimation(nameof(FrameworkElement.Width), from?.X, to.X, duration, ease, true));
-            AnimationFactories.Add(duration => TargetTransform.CreateDoubleAnimation(nameof(FrameworkElement.Height), from?.Y, to.Y, duration, ease, true));
+            if (!(TargetElement is FrameworkElement element)) throw new InvalidOperationException("Can't animate the size of an item that's not a framework element");
+            double?
+                fromX = double.IsNaN(element.Width) ? (double?)element.ActualWidth : from?.X,
+                fromY = double.IsNaN(element.Height) ? (double?)element.ActualHeight : from?.Y;
+            AnimationFactories.Add(duration => TargetTransform.CreateDoubleAnimation(nameof(FrameworkElement.Width), fromX, to.X, duration, ease, true));
+            AnimationFactories.Add(duration => TargetTransform.CreateDoubleAnimation(nameof(FrameworkElement.Height), fromY, to.Y, duration, ease, true));
 
             return this;
         }
