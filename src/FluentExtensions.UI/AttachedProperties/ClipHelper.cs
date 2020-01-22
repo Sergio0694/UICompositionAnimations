c96683ede89cad9b711aa8f8e1d6a67e40a44f3b
Sergio Pedri <sergio0694@live.com>
2020-01-23 00:46:00 +0100

    Added ClipHelper and FontIconExtension types

---
+++ src/FluentExtensions.UI/AttachedProperties/ClipHelper.cs
@@ -0,0 +1,52 @@
+﻿using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Hosting;
+
+#nullable enable
+
+namespace FluentExtensions.AttachedProperties
+{
+    /// <summary>
+    /// A <see langword="class"/> with an attached property to set a composition clip to <see cref="UIElement"/> instances
+    /// </summary>
+    public sealed class ClipHelper
+    {
+        /// <summary>
+        /// Gets the value of <see cref="ClipToBoundsProperty"/>
+        /// </summary>
+        /// <param name="element">The <see cref="UIElement"/> to read the property value from</param>
+        public static bool GetClipToBounds(UIElement element)
+        {
+            return (bool)element.GetValue(ClipToBoundsProperty);
+        }
+
+        /// <summary>
+        /// Sets the value of <see cref="ClipToBoundsProperty"/>
+        /// </summary>
+        /// <param name="element">The <see cref="UIElement"/> to set the property to</param>
+        /// <param name="value">The new value of the attached property</param>
+        public static void SetClipToBounds(UIElement element, bool value)
+        {
+            element.SetValue(ClipToBoundsProperty, value);
+        }
+
+        /// <summary>
+        /// A property that indicates whether or not the contents of the target <see cref="UIElement"/> should always be clipped to their parent's bounds
+        /// </summary>
+        public static readonly DependencyProperty ClipToBoundsProperty = DependencyProperty.RegisterAttached(
+            "ClipToBounds",
+            typeof(bool),
+            typeof(ClipHelper),
+            new PropertyMetadata(DependencyProperty.UnsetValue, OnClipToBoundsPropertyChanged));
+
+        private static void OnClipToBoundsPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UIElement @this = (UIElement)d;
+            bool value = (bool)e.NewValue;
+
+            Visual visual = ElementCompositionPreview.GetElementVisual(@this);
+
+            visual.Clip = value ? visual.Compositor.CreateInsetClip() : null;
+        }
+    }
+}
---
+++ src/FluentExtensions.UI/MarkupExtensions/FontIconExtension.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+
+#nullable enable
+
+namespace FluentExtensions.MarkupExtensions
+{
+    /// <summary>
+    /// A markup extension that can be used to create a <see cref="FontIcon"/> from a <see cref="string"/> that specifies a Segoe MDL2 Assets icon
+    /// </summary>
+    [MarkupExtensionReturnType(ReturnType = typeof(FontIcon))]
+    public sealed class FontIconExtension : MarkupExtension
+    {
+        /// <summary>
+        /// Gets or sets the <see cref="string"/> representing the icon to display
+        /// </summary>
+        public string? Glyph { get; set; }
+
+        /// <inheritdoc/>
+        protected override object ProvideValue()
+        {
+            return new FontIcon
+            {
+                Glyph = Glyph!,
+                FontFamily = new FontFamily("Segoe MDL2 Assets")
+            };
+        }
+    }
+}
