e154f4a69f90c19db5fe77a44ffdd64cae660076
Sergio Pedri <sergio0694@live.com>
2020-05-20 18:14:00 +0200

    Removed Win2D pipeline brushes
    Moved to Windows Community Toolkit: https://github.com/windows-toolkit/WindowsCommunityToolkit/pull/3112

--- UICompositionAnimations/Brushes/Effects/Interfaces/IPipelineEffect.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UICompositionAnimations.Brushes.Effects.Interfaces
-{
-    /// <summary>
-    /// The base <see langword="interface"/> for all the pipeline effects to be used in a <see cref="PipelineBrush"/>
-    /// </summary>
-    public interface IPipelineEffect { }
-}
--- UICompositionAnimations/Brushes/Effects/TileEffect.cs
+++
@@ -1,9 +0,0 @@
-﻿using UICompositionAnimations.Brushes.Effects.Abstract;
-
-namespace UICompositionAnimations.Brushes.Effects
-{
-    /// <summary>
-    /// An effect that loads an image and replicates it to cover all the available surface area
-    /// </summary>
-    public sealed class TileEffect : ImageEffectBase { }
-}
